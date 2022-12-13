6252b60155e947394a375803a8850b9eb293f430
Josh Tan <38924154+joshtn@users.noreply.github.com>
2022-12-13 23:03:18 +0100

    day01 1st star

--- 2022/day01/src/main.py
+++ 2022/day01/src/main.py
@@ -1,1 +1,20 @@
-print("hello aoc")
+def advent1():
+    f = open('../input.txt', 'r')
+    x = f.read().split("\n")
+    f.close()
+
+
+    largest_cal = 0
+    current_sum = 0
+    for _, value in enumerate(x):
+
+        if not value:
+            largest_cal = max(largest_cal, current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(value)
+
+
+    print(largest_cal)
+
+advent1()
