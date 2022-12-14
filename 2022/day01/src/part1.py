790dc5dc051be10126d564cb77679147cbcce260
Josh Tan <38924154+joshtn@users.noreply.github.com>
2022-12-14 19:54:07 +0100

    day01 file refactor

---
+++ 2022/day01/src/part1.py
@@ -0,0 +1,20 @@
+def advent1():
+    f = open('../demo-input.txt', 'r')
+    x = f.read().split("\n")
+    f.close()
+
+
+    largest_cal = 0
+    current_sum = 0
+    for _, value in enumerate(x):
+
+        if not value:
+            largest_cal = max(largest_cal, current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(value)
+
+
+    print(largest_cal)
+
+advent1()
