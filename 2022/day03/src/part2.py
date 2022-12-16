6c869356557c3bc68a860cd2445f6830ff1c3685
Josh Tan <38924154+joshtn@users.noreply.github.com>
2022-12-16 12:53:31 +0100

    part 2 pytest setup

--- 2022/day03/src/part2.py
+++ 2022/day03/src/part2.py
@@ -1,7 +1,12 @@
-def advent():
-    f = open('../input.txt', 'r')
-    s = f.read()
-    f.close()
+import argparse
+import os.path
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), '../input.txt')
+
+
+def compute(s: str) -> int:
 
     sum = 0
 
@@ -17,9 +22,40 @@ def advent():
             elif s.isupper():
                 sum += 27 + (ord(s) - ord('A'))
 
-    print(sum)
     return sum
 
 
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 70
 
-advent()
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
