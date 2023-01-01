8254fb9606966e5d8fe3c3efa5af9916265891ba
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-01-01 23:48:08 +0100

    day 7 part 1 done

--- 2022/day07/part1.py
+++ 2022/day07/part1.py
@@ -1,9 +1,11 @@
 import argparse
+from functools import lru_cache
 import os.path
 
 import pytest
 
 ####
+from collections import defaultdict
 
 
 
@@ -13,11 +15,67 @@ INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 # scan each line for number part  then add the number part to a total var.
 # until u reach a line that has dir or shifted to left..
 
-def compute(s: str) -> None:
-    s = s.split("\n")
+path = []
+dir_sizes = defaultdict(int) #defaultdict can manipulate when nothing is inside
+children = defaultdict(list)
 
-    for line in s:
-        print(line)
+
+def compute(s: str) -> int:
+    blocks = ("\n" + s.strip()).split("\n$ ")[1:]
+
+    for block in blocks:
+        parse(block)
+
+    ans = 0
+    for abspath in dir_sizes:
+        if dfs(abspath) <= 100000:
+            ans += dfs(abspath)
+            
+
+    return ans
+
+
+
+
+def parse(block):
+    
+    lines = block.split("\n")
+    command = lines[0]
+    outputs = lines[1:]
+
+    parts = command.split(" ")
+    op = parts[0]
+    if op == "cd":
+        if parts[1] == "..":
+            path.pop()
+        else:
+            path.append(parts[1])
+
+        return
+
+    abspath = "/".join(path)
+    assert op == "ls"
+
+    sizes = []
+    for line in outputs:
+        if not line.startswith("dir"):
+            sizes.append(int(line.split(" ")[0]))
+        else:
+            dir_name = line.split(" ")[1]
+            children[abspath].append(f"{abspath}/{dir_name}")
+
+    dir_sizes[abspath] = sum(sizes)
+
+
+@lru_cache(None)
+def dfs(abspath):
+    size = dir_sizes[abspath]
+    for child in children[abspath]:
+        size += dfs(child)
+    return size
+
+
+
 
 INPUT_S = '''\
 $ cd /
@@ -44,7 +102,7 @@ $ ls
 5626152 d.ext
 7214296 k
 '''
-EXPECTED = 7
+EXPECTED = 95437
 
 
 @pytest.mark.parametrize(
