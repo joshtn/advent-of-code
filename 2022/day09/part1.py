a541da3148e961fa90b328ae1dcad0f4eb18f031
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-01-08 23:56:49 +0100

    day 9 started

--- 2022/day09/part1.py
+++ 2022/day09/part1.py
@@ -9,31 +9,11 @@ import pytest
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-# grid with numbers each rep tree. Count trees visible
-# example input -> curr=[1][1] then check [0][1],[2][1],[1][0],[1],[2]
-# think: make a 2d array to rep grid. Class tree, then save state visible_left = bool
-# 1 make 2d list : find row_col_num append input to 2d list
-
+# is it a 2d grid similar to last days?
 
 
 def compute(s: str) -> int:
-    #s_split = s.split("\n")
-    #row_col_count = len(str(s_split[0]))
-    #print(row_col_count) 
-
-    grid = [list(map(int, line)) for line in s.splitlines()]
-
     t = 0
-
-    for r in range(len(grid)):
-        for c in range(len(grid[r])):
-            k = grid[r][c]
-            if (all(grid[r][x] < k for x in range(c)) or
-                    all(grid[r][x] < k for x in range(c + 1, len(grid[r]))) or
-                    all(grid[x][c] < k for x in range(r)) or
-                    all(grid[x][c] < k for x in range(r + 1, len(grid)))):
-                t += 1
-
     return t
 
 
@@ -42,11 +22,14 @@ def compute(s: str) -> int:
 
 
 INPUT_S = '''\
-30373
-25512
-65332
-33549
-35390
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
 '''
 EXPECTED = 21
 
