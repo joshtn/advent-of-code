81dea4c2e767d766ccf71f516e0e00c5e936ff99
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-01-12 12:45:50 +0100

    day 10 part 1

--- 2022/day10/part1.py
+++ 2022/day10/part1.py
@@ -12,58 +12,43 @@ INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 # count to act as timer, condition for the important  20th 60th + 40 each time 
 # input is simple to work with.
 # consider that instructions that different cycles to complete.
+# save values into stack and pop at right cycle count, no need
 
-hx, hy = 0, 0
-tx, ty = 0, 0
 
 
 def compute(s: str) -> int:
     lines = s.strip().split("\n")
 
+    X = 1
+    op = 0
+    ans = 0
 
-    def touching(x1, y1, x2, y2):
-        return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
-
-
-    def move(dx, dy):
-        global hx, hy, tx, ty
-
-        hx += dx
-        hy += dy
-
-        if not touching(hx, hy, tx, ty):
-            sign_x = 0 if hx == tx else (hx - tx) / abs(hx - tx)
-            sign_y = 0 if hy == ty else (hy - ty) / abs(hy - ty)
-
-            tx += sign_x
-            ty += sign_y
-
-
-    dd = {
-        "R": [1, 0],
-        "U": [0, 1],
-        "L": [-1, 0],
-        "D": [0, -1]
-    }
-
-
-    tail_visited = set()
-    tail_visited.add((tx, ty))
-
+    interesting = [20, 60, 100, 140, 180, 220]
 
     for line in lines:
-        op, amount = line.split(" ")
-        amount = int(amount)
-        dx, dy = dd[op]
+        parts = line.split(" ")
+        if parts[0] == "noop":
+            op += 1 
 
-        for _ in range(amount):
-            move(dx, dy)
-            tail_visited.add((tx, ty))
+            if op in interesting:
+                ans += op * X
+
+        elif parts[0] == "addx":
+            V = int(parts[1])
+            X += V
+
+            op += 1
+
+            if op in interesting:
+                ans += op * (X - V)
+
+            op += 1 
+
+            if op in interesting:
+                ans += op * (X - V)
 
 
-    print(len(tail_visited))
-            
-    return len(tail_visited)
+    return ans
 
 
 
@@ -71,16 +56,11 @@ def compute(s: str) -> int:
 
 
 INPUT_S = '''\
-R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
+noop
+addx 3
+addx -5
 '''
-EXPECTED = 13
+EXPECTED = -1
 
 
 @pytest.mark.parametrize(
