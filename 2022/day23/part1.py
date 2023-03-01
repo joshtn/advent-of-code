b9530d2b9d972034ca40e5194949cc062a48dc9c
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-03-01 23:57:31 +0100

    day 23 part 1

--- 2022/day23/part1.py
+++ 2022/day23/part1.py
@@ -4,13 +4,67 @@ import os.path
 import pytest
 
 ####
-import re
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    return (1000 * (r + 1) + 4 * (c + 1) + k)
+    elves = set()
+
+    for r, line in enumerate(s.splitlines()):
+        for c, item in enumerate(line):
+            if item == "#":
+                elves.add(c + r * 1j)
+
+    scanmap = {
+        -1j: [-1j - 1, -1j, -1j + 1],
+        1j: [1j - 1, 1j, 1j + 1],
+        1: [1 - 1j, 1, 1 + 1j],
+        -1: [-1 - 1j, -1, -1 + 1j]
+    }
+
+    moves = [-1j, 1j, -1, 1]
+    N = [-1 - 1j, -1j, -1j + 1, 1, 1 + 1j, 1j, 1j - 1, -1]
+
+    for _ in range(10):
+        once = set()
+        twice = set()
+
+        for elf in elves:
+            if all(elf + x not in elves for x in N):
+                continue
+            for move in moves:
+                if all(elf + x not in elves for x in scanmap[move]):
+                    prop = elf + move
+                    if prop in twice:
+                        pass
+                    elif prop in once:
+                        twice.add(prop)
+                    else:
+                        once.add(prop)
+                    break
+
+        ec = set(elves)
+
+        for elf in ec:
+            if all(elf + x not in ec for x in N):
+                continue
+            for move in moves:
+                if all(elf + x not in ec for x in scanmap[move]):
+                    prop = elf + move
+                    if prop not in twice:
+                        elves.remove(elf)
+                        elves.add(prop)
+                    break
+        
+        moves.append(moves.pop(0))
+
+    mx = min(x.real for x in elves)
+    Mx = max(x.real for x in elves)
+    my = min(x.imag for x in elves)
+    My = max(x.imag for x in elves)
+
+    return ((Mx - mx + 1) * (My - my + 1) - len(elves))
 
 
 
