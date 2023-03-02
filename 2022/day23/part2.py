121ca1229cb71978c04270d7425163bd394d5273
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-03-02 11:43:49 +0100

    day 23 part 2

--- 2022/day23/part2.py
+++ 2022/day23/part2.py
@@ -4,108 +4,71 @@ import os.path
 import pytest
 
 ####
-import re
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
+    elves = set()
 
-    grid = []
-    done = False
+    for r, line in enumerate(s.splitlines()):
+        for c, item in enumerate(line):
+            if item == "#":
+                elves.add(c + r * 1j)
 
-    for line in s.splitlines(True):
-        line = line[:-1]
-        if line == "":
-            done = True
-        if done:
-            sequence = line
-        else:
-            grid.append(line)
+    scanmap = {
+        -1j: [-1j - 1, -1j, -1j + 1],
+        1j: [1j - 1, 1j, 1j + 1],
+        1: [1 - 1j, 1, 1 + 1j],
+        -1: [-1 - 1j, -1, -1 + 1j]
+    }
 
-    width = max(map(len, grid))
-    grid = [line + " " * (width - len(line)) for line in grid]
+    moves = [-1j, 1j, -1, 1]
+    N = [-1 - 1j, -1j, -1j + 1, 1, 1 + 1j, 1j, 1j - 1, -1]
 
-    r = 0
-    c = 0
-    dr = 0
-    dc = 1
+    last = set(elves)
 
-    while grid[r][c] != ".":
-        c += 1
+    iter = 1
+    while True:
+        once = set()
+        twice = set()
 
-    for x, y in re.findall(r"(\d+)([RL]?)", sequence):
-        x = int(x)
-        for _ in range(x):
-            cdr = dr
-            cdc = dc
-            nr = r + dr
-            nc = c + dc
+        for elf in elves:
+            if all(elf + x not in elves for x in N):
+                continue
+            for move in moves:
+                if all(elf + x not in elves for x in scanmap[move]):
+                    prop = elf + move
+                    if prop in twice:
+                        pass
+                    elif prop in once:
+                        twice.add(prop)
+                    else:
+                        once.add(prop)
+                    break
 
-            if nr < 0 and 50 <= nc < 100 and dr == -1:
-                dr, dc = 0, 1 
-                nr, nc = nc + 100, 0
-            elif nc < 0 and 150 <= nr < 200 and dc == -1:
-                dr, dc = 1, 0
-                nr, nc = 0, nr - 100
-            elif nr < 0 and 100 <= nc < 150 and dr == -1:
-                nr, nc = 199, nc - 100
-            elif nr >= 200 and 0 <= nc < 50 and dr == 1:
-                nr, nc = 0, nc + 100
-            elif nc >= 150 and 0 <= nr < 50 and dc == 1:
-                dc = -1
-                nr, nc = 149 - nr, 99
-            elif nc == 100 and 100 <= nr < 150 and dc == 1:
-                dc = -1
-                nr, nc = 149 - nr, 149
-            elif nr == 50 and 100 <= nc < 150 and dr == 1:
-                dr, dc = 0, -1
-                nr, nc = nc -  50, 99
-            elif nc == 100 and 50 <= nr < 100 and dc == 1:
-                dr, dc = -1, 0
-                nr, nc = 49, nr + 50
-            elif nr == 150 and 50 <= nc < 100 and dr == 1:
-                dr, dc = 0, -1
-                nr, nc = nc + 100, 49
-            elif nc == 50 and 150 <= nr < 200 and dc == 1:
-                dr, dc = -1, 0
-                nr, nc = 149, nr - 100
-            elif nr == 99 and 0 <= nc < 50 and dr == -1:
-                dr, dc = 0, 1 
-                nr, nc = nc + 50, 50
-            elif nc == 49 and 50 <= nr < 100 and dc == -1:
-                dr, dc = 1, 0
-                nr, nc = 100, nr - 50
-            elif nc == 49 and 0 <= nr < 50 and dc == -1:
-                dc = 1 
-                nr, nc = 149 - nr, 0
-            elif nc < 0 and 100 <= nr < 150 and dc == -1:
-                dc = 1 
-                nr, nc = 149 - nr, 50
+        ec = set(elves)
 
-            if grid[nr][nc] == "#":
-                dr = cdr
-                dc = cdc
-                break
-            r = nr
-            c = nc
-        if y == "R":
-            dr, dc = dc, -dr
-        elif y == "L":
-            dr, dc = -dc, dr
+        for elf in ec:
+            if all(elf + x not in ec for x in N):
+                continue
+            for move in moves:
+                if all(elf + x not in ec for x in scanmap[move]):
+                    prop = elf + move
+                    if prop not in twice:
+                        elves.remove(elf)
+                        elves.add(prop)
+                    break
+        
+        moves.append(moves.pop(0))
 
-    if dr == 0:
-        if dc == 1:
-            k = 0
-        else:
-            k = 2
-    else:
-        if dr == 1:
-            k = 1
-        else:
-            k = 3
+        if last == elves:
+            break
 
-    return (1000 * (r + 1) + 4 * (c + 1) + k)
+        last = set(elves)
+        iter += 1
+
+    return iter
 
 
 
@@ -113,22 +76,15 @@ def compute(s: str) -> int:
 
 
 INPUT_S = '''\
-        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5
+....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..
 '''
-EXPECTED = 6032
+EXPECTED = 110
 
 
 @pytest.mark.parametrize(
